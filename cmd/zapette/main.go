30b796b60c9a2b36be8a9586731d8cc0df967afa
Pierre Peltier <pierre.peltier@protonmail.com>
2024-08-30 19:58:08 +0200

    Use the package flag instead of viper and cobra (#38)
    * Use the package flag instead of viper and cobra
    
    This allow to remove a lot of dependencies for the same features
    
    * Add the version flag
    
    * Add the help flag

--- cmd/zapette/main.go
+++ cmd/zapette/main.go
@@ -1,41 +1,134 @@
 package main
 
 import (
+	"context"
+	"flag"
+	"fmt"
+	"io"
 	"os"
+	"path"
 
-	"github.com/Peltoche/zapette/cmd/zapette/commands"
+	"github.com/Peltoche/zapette/internal/server"
 	"github.com/Peltoche/zapette/internal/tools/buildinfos"
-	"github.com/spf13/cobra"
+	"github.com/adrg/xdg"
 )
 
-const binaryName = "zapette"
+const (
+	binaryName        = "zapette"
+	binaryDescription = `Observe and manager your server.
+
+Usage:
+  ` + binaryName + ` [flags...]
+
+Flags:
+`
+)
+
+var configDirs = append(xdg.DataDirs, xdg.DataHome)
 
 type exitCode int
 
 const (
-	exitOK    exitCode = 0
-	exitError exitCode = 1
+	exitOK        exitCode = 0
+	exitError     exitCode = 1
+	exitInitError exitCode = 2
 )
 
 func main() {
-	code := mainRun()
+	output := os.Stdout
+
+	code := mainRun(os.Args, output)
 	os.Exit(int(code))
 }
 
-func mainRun() exitCode {
-	cmd := &cobra.Command{
-		Use:     binaryName,
-		Short:   "Observe and manage your server.",
-		Version: buildinfos.Version(),
+func mainRun(args []string, output io.Writer) exitCode {
+	ctx := context.Background()
+
+	defaultFolder := getDefaultFolder()
+	flags, err := parseFlags(args, defaultFolder, output)
+	if err != nil {
+		return exitInitError
 	}
 
-	// Subcommands
-	cmd.AddCommand(commands.NewRunCmd(binaryName))
+	if flags == nil {
+		return exitOK
+	}
 
-	err := cmd.Execute()
+	if flags.PrintVersion {
+		fmt.Fprintf(output, "%s version %s\n", binaryName, buildinfos.Version())
+		return exitOK
+	}
+
+	cfg, err := NewConfigFromFlags(flags)
+	if err != nil {
+		io.WriteString(output, err.Error())
+		return exitInitError
+	}
+
+	_, err = server.Run(ctx, cfg)
 	if err != nil {
 		return exitError
 	}
 
 	return exitOK
+}
+
+func getDefaultFolder() string {
+	var defaultFolder string
+
+	for _, dir := range configDirs {
+		_, err := os.Stat(path.Join(dir, binaryName))
+		if err == nil {
+			defaultFolder = path.Join(dir, binaryName)
+			break
+		}
+	}
+
+	if defaultFolder == "" {
+		defaultFolder = path.Join(xdg.DataHome, binaryName)
+	}
+
+	return defaultFolder
+}
+
+func parseFlags(args []string, defaultFolder string, output io.Writer) (*flags, error) {
+	flags := flags{}
+
+	fs := flag.NewFlagSet("flags", flag.ContinueOnError)
+	fs.SetOutput(output)
+
+	if !buildinfos.IsRelease() {
+		// Those flags are  only available outside the releases for security reasons.
+		fs.BoolVar(&flags.Dev, "dev", false, "Run in dev mode and make json prettier")
+		fs.BoolVar(&flags.HotReload, "hot-reload", false, "Enable the asset hot reload")
+	}
+
+	fs.BoolVar(&flags.Debug, "debug", false, "Force the debug level")
+	fs.StringVar(&flags.LogLevel, "log-level", "info", "Log message verbosity LEVEL (debug, info, warning, error)")
+
+	fs.StringVar(&flags.Folder, "folder", defaultFolder, "Specify you data directory location")
+	fs.BoolVar(&flags.MemoryFS, "memory-fs", false, "Replace the OS filesystem by a in-memory stub. *Every data will disapear after each restart*.")
+
+	fs.StringVar(&flags.TLSCert, "tls-cert", "", "Public HTTPS certificate file (.crt)")
+	fs.StringVar(&flags.TLSKey, "tls-key", "", "Private HTTPS key file (.key)")
+	fs.BoolVar(&flags.SelfSignedCert, "self-signed-cert", false, "Generate and use a self-signed HTTPS/TLS certificate.")
+
+	fs.IntVar(&flags.HTTPPort, "http-port", 5764, "Web server port number.")
+	fs.StringVar(&flags.HTTPHost, "http-host", "0.0.0.0", "Web server IP address")
+
+	fs.BoolVar(&flags.PrintVersion, "version", false, "version for zapette")
+	fs.BoolVar(&flags.PrintHelp, "help", false, "help for zapette")
+
+	err := fs.Parse(args[1:])
+	if err != nil {
+		return nil, err
+	}
+
+	if flags.PrintHelp {
+		io.WriteString(output, binaryDescription)
+		fs.PrintDefaults()
+		return nil, nil
+	}
+
+	return &flags, nil
 }
